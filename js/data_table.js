f4532824b6a9eb77811cc682da7dd23e01331a27
AnaKelyna <64177211+AnaKelyna@users.noreply.github.com>
2020-09-22 01:38:58 -0500

    Delete data_table.js

--- js/data_table.js
+++
@@ -1,8 +0,0 @@
-d3.csv("Web-Design-Challenge/data/cities.csv").then(function(citiesData) {
-
-    // Step 1: Parse Data/Cast as numbers
-    // ==============================
-    citiesData.forEach(function(data) {
-        data.City_ID = +data.City_ID;
-    });
-});
